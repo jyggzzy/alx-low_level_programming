16b93269b28164b82ab4cd5e68e07f08cf5c7cf1
jyggzzy <jolayemigoodness@gmail.com>
2022-07-05 15:32:24 -0700

    origin

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  * main - prints _putchar
  * Description: prints _putchar
@@ -7,15 +9,22 @@
  *
  */
 
+
 int main(void)
+
 {
 	char text[9] = "_putchar";
 
 	int i = 0;
 
+
+
 	for (i = 0; i < 8; i++)
+
 	{
+
 		_putchar(text[i]);
+
 	}
 
 	_putchar('\n');
