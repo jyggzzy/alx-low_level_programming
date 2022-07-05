4e7a23971bc85085094950a667078fe511af2bee
jyggzzy <jolayemigoodness@gmail.com>
2022-07-05 16:36:35 -0700

    origin

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * print_alphabet_x10.c - prints the alphabets in lower case ten times
+ * Return: 0 (Success
+ *
+ */
+
+int a, b;
+{
+	for (a = 0, a <= 9, a++);
+	{
+		for (b = 97, b <=122, b++);
+		{
+			_putchar(b);
+		}
+
+	_putchar('\n');
+	}
+
+	return (0)
+}
